ac4335eda1ebd29214502bb18358e2e34a231fa2
Minhan-Bae <yuyuqw92@gmail.com>
2021-05-19 23:27:09 +0900

    Practice_opencv_19th May 2021

---
+++ Chapter 01.py
@@ -0,0 +1,16 @@
+# Import image
+
+import cv2 as cv
+print("Package Imported")
+
+
+# img = cv.imread("original.jpg")
+# cv.imshow("Original",img)
+# cv.waitKey(0)
+
+cap = cv.VideoCapture("original.mp4")
+while True:
+  success, img = cap.read()
+  cv.imshow("Video",img)
+  if cv.waitKey(1) & 0xFF ==ord('q'):
+    break
---
+++ Chapter 02.py
@@ -0,0 +1,21 @@
+# Basic function
+
+import cv2 as cv
+import numpy as np
+
+original_img = cv.imread("original.jpg")
+img = cv.resize(original_img, dsize=(768,432))
+kernel = np.ones((5,5),np.uint8)
+
+imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+imgBlur = cv.GaussianBlur(imgGray,(7,7),0)
+imgCann = cv.Canny(img,150,200)
+imgDial = cv.dilate(imgCann,kernel,iterat ions=1)
+imgErod = cv.erode(imgDial,kernel,iterations=1)
+
+cv.imshow("Gray Image",imgGray)
+cv.imshow("Blur Image",imgBlur)
+cv.imshow("Cann Image",imgCann)
+cv.imshow("Dial Image",imgDial)
+cv.imshow("Erod Image",imgErod)
+cv.waitKey(0)
