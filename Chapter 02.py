861e854a6288ffcd3967999c837ac111f3d876de
Minhan Bae <yuyuqw92@gmail.com>
2021-05-30 14:54:52 +0900

    Practice_opencv_30th May 2021

--- Chapter 02.py
+++ Chapter 02.py
@@ -10,7 +10,7 @@ kernel = np.ones((5,5),np.uint8)
 imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 imgBlur = cv.GaussianBlur(imgGray,(7,7),0)
 imgCann = cv.Canny(img,150,200)
-imgDial = cv.dilate(imgCann,kernel,iterat ions=1)
+imgDial = cv.dilate(imgCann,kernel,iterations=1)
 imgErod = cv.erode(imgDial,kernel,iterations=1)
 
 cv.imshow("Gray Image",imgGray)
